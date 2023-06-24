c44c4cdd4d45be05f11e4b056637c949cbeaa289
Inunu1 <inunu1@outlook.com>
2023-06-25 00:41:26 +0900

    spring initilizerにて作成したものをコミット

---
+++ estimateMiniToto/src/main/java/com/Inunu1/estimateMiniToto/EstimateMiniTotoApplication.java
@@ -0,0 +1,13 @@
+package com.Inunu1.estimateMiniToto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstimateMiniTotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstimateMiniTotoApplication.class, args);
+	}
+
+}
---
+++ estimateMiniToto/src/test/java/com/Inunu1/estimateMiniToto/EstimateMiniTotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Inunu1.estimateMiniToto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstimateMiniTotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
