22e7efe8b78b7cceb29632a8c09c9b39024f6a5c
Inunu1 <inunu1@outlook.com>
2023-06-27 02:11:22 +0900

    途中だがコミット

--- estimateMiniToto/src/main/java/com/Inunu1/estimateMiniToto/controller/SystemController.java
+++ estimateMiniToto/src/main/java/com/Inunu1/estimateMiniToto/controller/SystemController.java
@@ -1,4 +1,21 @@
 package com.Inunu1.estimateMiniToto.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class SystemController {
+
+    //チームをスクレイピングするコントローラー
+    @GetMapping({"/", "/index"})
+    public String getDefaultAccess(Model model) {
+        return "index";
+    }
+    //試合結果をスクレイピングするコントローラー
+
+
+    //レーティング計算を行うコントローラー
+
 }
+
